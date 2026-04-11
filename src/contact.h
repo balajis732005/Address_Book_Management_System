ba125fad367ba1d38e024c0510fdccb422262ccc
balajis732005 <balajis07032005@gmail.com>
2026-04-11 10:53:16 +0530

    Added contact.h

---
+++ src/contact.h
@@ -0,0 +1,19 @@
+// Maximum contacts to store
+#define MAX_CONTACT 100
+
+#define USERNAME_SIZE 31
+#define USERPHONENUMBER_SIZE 11
+#define USEREMAILID_SIZE 51
+
+// Single User Contact
+typedef struct {
+    char userName[USERNAME_SIZE];
+    char userPhoneNumber[USERPHONENUMBER_SIZE];
+    char userEmailId[USEREMAILID_SIZE];
+} Contact;
+
+// Address Book
+typedef struct {
+    Contact contactsBook[MAX_CONTACT];
+    int contactCount; // No.of Contacts stored
+} AddressBook;
