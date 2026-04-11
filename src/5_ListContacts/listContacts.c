b3a4147113fe81d456d1bdc754fd8ca9b70c6efe
balajis732005 <balajis07032005@gmail.com>
2026-04-11 10:54:58 +0530

    Added List Contacts feature

---
+++ src/5_ListContacts/listContacts.c
@@ -0,0 +1,15 @@
+#include "listContacts.h"
+
+void listContacts(AddressBook *addressBook){
+    printf("\nContact List: \n");
+    int iter = 0;
+
+    while(iter < addressBook->contactCount){
+        printf("\nContact %d:\n", (iter + 1));
+        printf("Name: %s\n", ((addressBook->contactsBook)[iter]).userName);
+        printf("PhoneNumber: %s\n", ((addressBook->contactsBook)[iter]).userPhoneNumber);
+        printf("EmailId: %s\n", ((addressBook->contactsBook)[iter]).userEmailId);
+
+        iter++;
+    }
+}
